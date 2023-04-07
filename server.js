8dbd6c7d56dc680a478eb558c502bffa0f705919
mira <medkit@centrum.cz>
2023-04-07 21:11:22 +0200

    test

---
+++ server.js
@@ -0,0 +1,19 @@
+//var express = require('express');
+//var app = express();
+//const path = require('path');
+//const PORT = process.env.PORT || 5000;
+//var app_path = '../dist/frontend';
+
+//app.use('/', express.static(path.join(__dirname, app_path)))
+//.listen(PORT, (console.log( 'jede')));
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/frontend'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/frontend/index.html'));});
+app.listen(process.env.PORT || 8080);
+
--- server/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-var app_path = '../dist/frontend';
-
-app.use('/', express.static(path.join(__dirname, app_path)))
-.listen(PORT, (console.log( 'jede')));
-
