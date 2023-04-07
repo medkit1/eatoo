b4fc490670021d72b0fb657b8fc43a9d92c49fc3
mira <medkit@centrum.cz>
2023-04-07 23:23:44 +0200

    t

--- src/app/server.js
+++ src/app/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static('../../dist/frontend'));
 app.get('/*', function(req,res) {
 //res.sendFile(path.join(__dirname + '../../../../dist/frontend/index.html'));});
-res.sendFile('index.html', {root: 'dist/frontend/'});});
+res.sendFile('index.html', {root: './dist/frontend/'});});
 app.listen(process.env.PORT || 8080);
--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const path = require('path');
 const app = express();
 app.use(express.static('../dist/frontend'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '../../../dist/frontend/index.html'));});
+//res.sendFile(path.join(__dirname + '../../../dist/frontend/index.html'));});
+res.sendFile('index.html', {root: './dist/frontend/'});});
 app.listen(process.env.PORT || 8080);
 
