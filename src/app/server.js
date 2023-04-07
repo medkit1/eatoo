d53403d666bc6e72f24cf9b9c9657c00f7849f6b
mira <medkit@centrum.cz>
2023-04-07 23:00:38 +0200

    t

--- src/app/server.js
+++ src/app/server.js
@@ -3,5 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static('../../dist/frontend'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join('../../dist/frontend/index.html'));});
+res.sendFile(path.join(__dirname + '../../../../dist/frontend/index.html'));});
 app.listen(process.env.PORT || 8080);
